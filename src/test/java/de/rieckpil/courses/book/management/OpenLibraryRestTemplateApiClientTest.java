ebccc8c4b43297f76e1851697598be5cda4ee3e4
Riju Vijayan <rijuvijayan@gmail.com>
2023-01-11 22:34:25 +1300

    feat: OpenLibraryRestTemplateApiClientTest.java implemented

--- src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
+++ src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.match.MockRestRequestMatchers;
@@ -12,8 +13,9 @@ import org.springframework.test.web.client.response.MockRestResponseCreators;
 import org.springframework.web.client.HttpServerErrorException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
 @RestClientTest(OpenLibraryRestTemplateApiClient.class)
 class OpenLibraryRestTemplateApiClientTest {
@@ -28,21 +30,47 @@ class OpenLibraryRestTemplateApiClientTest {
 
   @Test
   void shouldInjectBeans() {
+    assertNotNull(cut);
+    assertNotNull(mockRestServiceServer);
+
   }
 
   @Test
   void shouldReturnBookWhenResultIsSuccess() {
+    this.mockRestServiceServer
+      .expect(requestTo(String.format("/api/books?jscmd=data&format=json&bibkeys=ISBN:%s",ISBN)))
+      .andRespond(withSuccess(new ClassPathResource(String.format("/stubs/openlibrary/success-%s.json", ISBN))
+        , MediaType.APPLICATION_JSON));
+
+    Book book = cut.fetchMetadataForBook(ISBN);
+    assertNotNull(book);
+    assertNull(book.getId());
   }
 
   @Test
   void shouldReturnBookWhenResultIsSuccessButLackingAllInformation() {
+
   }
 
   @Test
   void shouldPropagateExceptionWhenRemoteSystemIsDown() {
+    this.mockRestServiceServer
+      .expect(requestTo(String.format("/api/books?jscmd=data&format=json&bibkeys=ISBN:%s",ISBN)))
+      .andRespond(withStatus(HttpStatus.BAD_GATEWAY));
+
+    assertThrows(HttpServerErrorException.class, () -> cut.fetchMetadataForBook(ISBN));
   }
 
   @Test
   void shouldContainCorrectHeadersWhenRemoteSystemIsInvoked() {
+    this.mockRestServiceServer
+      .expect(requestTo(String.format("/api/books?jscmd=data&format=json&bibkeys=ISBN:%s", ISBN)))
+      .andExpect(header("X-Custom-Auth", "Duke42"))
+      .andExpect(header("X-Customer-Id", "42"))
+      .andRespond(withSuccess(new ClassPathResource(String.format("/stubs/openlibrary/success-%s.json", ISBN))
+        , MediaType.APPLICATION_JSON));
+
+    Book book = cut.fetchMetadataForBook(ISBN);
+    assertNotNull(book);
   }
 }
